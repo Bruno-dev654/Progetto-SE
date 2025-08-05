54ba886e6c3bc3a5eb5d91732b6a4361a3c45768
Bruno <marcobruno102@gmail.com>
2025-08-05 17:19:08 +0200

    Primo commit

---
+++ src/main/java/com/example/Progetto/ProgettoSeApplication.java
@@ -0,0 +1,12 @@
+package com.example.Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSeApplication.class, args);
+	}
+}
